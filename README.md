192cbc7a7bc3451be018c000989a772945d3d55f
devio-at <github@devio.at>
2022-11-25 18:01:48 +0100

    init code, data

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # german-ipa-dict
 German IPA dictionary as extracted from wiktionary
+
+## Generate de.csv
+
+- Download `enwiktionary-****-pages-meta-current.xml` dump from [Wikimedia](https://dumps.wikimedia.org/backup-index.html)
+- `$ python3 ./extract_de_ipa.py  > de_raw.csv`
+- `$ sort < de_raw.csv > de.csv`
---
+++ extract_de_ipa.py
@@ -0,0 +1,60 @@
+pageTitle = ""
+pronFound = False
+
+format = "csv"
+
+with open('../../jisho/enwiktionary-20220120-pages-meta-current.xml', newline='') as f:
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        
+        if line.find("</page>") > -1:
+            pageTitle = ""
+            pronFound = False
+            continue
+
+        if line.find("<title>") > -1:
+            pt = line.replace("<title>", "").replace("</title>", "").strip()
+            if pt.find(":") > -1:    # skip pages in namespaces
+                continue
+
+            pageTitle = pt
+            continue
+
+        if pageTitle == "":
+            continue
+
+        if line.startswith("===Pronunciation==="):
+            pronFound = True
+        elif line.startswith("=="):
+            pronFound = False
+
+        if line.startswith("=="):
+            continue
+        if not pronFound:
+            continue
+
+        if line.startswith("* {{IPA|de|") or line.startswith("** {{IPA|de|"):
+            parts = line.strip().split("{{")
+            ipa = parts[1].replace("IPA|de|", "").replace("}}", "").replace("|", ",").strip()
+
+            if format == "all":
+                print("page: " + pageTitle + "; IPA de: " + ipa
+                    + (("; " + (", ".join(
+                        map(lambda p: p.replace("|", ":").replace("}}", "").strip(), parts[2:]))))
+                        if len(parts) > 2 else ""))
+
+            if format == "csv":
+                print(pageTitle + "," 
+                    + (ipa if ipa.find(",")==-1 else ("\"" + ipa + "\""))
+                    + ((",\"" 
+                        + (", ".join(
+                            map(lambda p: 
+                                p.replace("|", ":").replace("}}", "").replace("\"", "'").strip(), 
+                            parts[2:])))
+                        + "\"")
+                        if len(parts) > 2 else "")
+                )
+
+f.close()
